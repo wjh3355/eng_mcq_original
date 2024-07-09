0835470e04e2ef36ea36d9072475ce93bcaf9a57
jhw3355 <jhwang0324@gmail.com>
2024-07-09 23:44:16 +0800

    stuff

--- js/script.js
+++ js/script.js
@@ -24,25 +24,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 	$ajaxUtils.sendGetRequest(jsonSource, function (responseArray) {
 		// console.log("Fetching question array...");
-
 		try {
-
 			if (!Array.isArray(responseArray) || responseArray.length === 0) {
 				throw new Error("Invalid or empty response from server.");
 			}
-
 			qnsObjArray = responseArray;
 			shuffleArray(qnsObjArray);
 			var orderOfQns = qnsObjArray.map(qnObj => qnObj.qnNum);
 			// console.log(`Shuffled order of questions: \n${orderOfQns}`);
-
 			initialiseQuestion();
-
 		} catch (error) {
 			// console.error(error.message);
 			alert("Failed to load questions. Please try again later.");
 		}
-
 	}, function (error) {
 		// console.error("Failed to fetch question data:", error);
 		alert("Failed to fetch questions. Please check your internet connection.");
@@ -54,17 +48,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // initialises loop
 function initialiseQuestion() {
 	displayLoading();
-
 	if (qnsObjArrayPtr === qnsObjArray.length) {
 		qnsObjArrayPtr = 0;
 		// console.log("(End of questions, looping back to first)");
 	};
-
-	// console.log("Initialising qn / Next button clicked...");
-
+	// console.log("%cInitialising qn / Next button clicked...", 'font-weight: bold;');
 	nextQnButton.disabled = true;
 	dispExplButton.disabled = true;
-
 	qnObj = qnsObjArray[qnsObjArrayPtr];
 
 	resetAll();
@@ -72,30 +62,23 @@ function initialiseQuestion() {
 	displayQnSentence();
 	assignOptionsRandomly();
 	enableOptions();
-
 	// console.log("Waiting for option button input...");
-
 	qnsObjArrayPtr++;
 };
 
 // displays sentence
 function displayQnSentence() {
 	var { sentence, wordToTest } = qnObj;
-
 	// console.log("Next question:");
 	// console.table(qnObj);
-
 	sentence = sentence.replace(wordToTest, `<strong>${wordToTest}</strong>`);
-
 	sentenceElement.innerHTML = `<p class="my-2">${sentence}</p>`;
-
 	// console.log("Sentence displayed");
 };
 
 // inserts explanation
 function insertExplanation() {
 	var { expl: { rootWord, type, def } } = qnObj;
-
 	collapsibleExplElement.innerHTML = 
 		`
 		<div class="card card-body">
@@ -110,7 +93,6 @@ function insertExplanation() {
 			<p class="mb-0">${def}.</p>
 		</div>
 		`;
-
 	// console.log("Explanation inserted");
 };
 
@@ -124,11 +106,8 @@ function shuffleArray(array) {
 
 // refreshes accumulative score
 function updateScore() {
-	if (numQnsAttempted === 0) {
-		return;
-	}
+	if (numQnsAttempted === 0) {return}
 	var percentCorrect = (numCorrectAns/numQnsAttempted)*100;
-
 	scoreElement.textContent = `${numCorrectAns} / ${numQnsAttempted} (${Math.round(percentCorrect)}%)`;
 	// console.log("Updated score");
 };
@@ -146,20 +125,17 @@ function displayLoading() {
 // assign options randomly to buttons, mark correct and wrong
 function assignOptionsRandomly() {
 	var { options, correctAns } = qnObj;
-
 	shuffleArray(options);
-
 	optionButtons.forEach((button, idx) => {
 		var option = options[idx];
 		button.innerHTML = `<p class="m-0">${option}</p>`;
-
 		if (option === correctAns) {
-			// Correct option
+			// CORRECT OPTION
 			correctAnsButton = button;
 			button.addEventListener("click", correctAnsHandler);
 			// console.log(`Correct answer is button ${idx+1} (${option})`);
 		} else {
-			// Wrong option
+			// WRONG OPTION
 			button.addEventListener("click", wrongAnsHandler);
 		}
 	});
@@ -173,7 +149,6 @@ function enableOptions() {
 		button.disabled = false;
 		button.classList.add("option-enabled-border");
 	});
-
 	// console.log("Options enabled");
 };
 
@@ -183,7 +158,6 @@ function disableOptions() {
 		button.disabled = true;
 		button.classList.remove("option-enabled-border");
 	});
-
 	// console.log("Options disabled");
 };
 
@@ -191,18 +165,13 @@ function disableOptions() {
 function resetAll() {
 	optionButtons.forEach(button => {
 		button.classList = "option";
-		button.textContent = "";
 		button.innerHTML = "";
 		button.removeEventListener("click", correctAnsHandler);
 		button.removeEventListener("click", wrongAnsHandler);
 	});
-
-	collapsibleExplElement.textContent = "";
 	collapsibleExplElement.innerHTML = "";
 	collapsibleExplElement.classList.remove("show");
-
 	dispExplButton.textContent = "Show Explanation";
-
 	correctAnsButton = null;
 	// console.log("Options and explanation reset");
 };
@@ -211,14 +180,11 @@ function resetAll() {
 
 // handles correct answer
 function correctAnsHandler() {
-	// console.log(`Correct ans clicked (${this.textContent})`);
-
+	// console.log(`%cCorrect ans clicked (${this.textContent})`, 'color: green; font-weight: bold;');
 	numQnsAttempted++;
 	numCorrectAns++;
-
 	this.classList.add("greenBorder");
 	this.insertAdjacentHTML("beforeend", tickIcon);
-
 	insertExplanation();
 	disableOptions();
 	nextQnButton.disabled = false;
@@ -228,16 +194,12 @@ function correctAnsHandler() {
 
 // handles wrong answer
 function wrongAnsHandler() {
-	// console.log(`Wrong ans clicked (${this.textContent})`);
-
+	// console.log(`%cWrong ans clicked (${this.textContent})`, 'color: red; font-weight: bold;');
 	numQnsAttempted++;
-
 	this.classList.add("redBorder");
 	this.insertAdjacentHTML("beforeend", crossIcon);
-
 	correctAnsButton.classList.add("greenBorder");
 	correctAnsButton.insertAdjacentHTML("beforeend", tickIcon);
-
 	insertExplanation();
 	disableOptions();
 	nextQnButton.disabled = false;
